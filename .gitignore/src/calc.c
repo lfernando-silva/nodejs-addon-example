6beb0c7505e555eba19ce639b543da1d0bc57e32
Luiz Fernando <lfernando-silva@hotmail.com>
2018-01-31 13:49:56 -0200

    Add files via upload

---
+++ .gitignore/src/calc.c
@@ -0,0 +1,15 @@
+double soma(double n1, double n2){
+    return n1 + n2;
+}
+
+double subtrai(double n1, double n2) {
+	return n1 - n2;
+}
+
+double multiplica(double n1, double n2) {
+	return n1 * n2;
+}
+
+double divide(double n1, double n2) {
+	return n1 / n2;
+}
